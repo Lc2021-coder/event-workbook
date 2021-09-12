33639b5f25a39d0008ea049975e5f02d5771b1da
Lc2021-coder <81456231+Lc2021-coder@users.noreply.github.com>
2021-09-11 21:25:33 -0700

    Update app.js

--- change/app.js
+++ change/app.js
@@ -1,9 +1,9 @@
-var input = document.getElementById("change-me");
+var input = document.getElementById("input");
 // select our input by ID and assign it to the variable, "input"
 
 // use the add event listener method to put an event on "input"
 // "change" is one of a few dozen strings and add event listener will understand. It's our first argument 
-input.addEventListener("change", function(e){ // we pass an entire function as the second argument. 
+input.addEventListener("change"(e){ // we pass an entire function as the second argument. 
     document.getElementById("output").textContent = e.target.value
     // select the div, and change the "textContent" property to the value of the events target (the input box)
 })
